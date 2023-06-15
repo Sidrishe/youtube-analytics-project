63724b8fa7a79d9f061f176adc17d5f814008558
Mikhail <osipov.m93@mail.ru>
2023-06-15 17:03:41 +0300

    add class

---
+++ src/video.py
@@ -0,0 +1,29 @@
+import os
+from googleapiclient.discovery import build
+
+class Video:
+    """Класс для видео на ютубе"""
+
+    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+    api_key: str = os.getenv('YT_API_KEY')
+
+    # создать специальный объект для работы с API
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id: str) -> None:
+        self.video_id = video_id
+        self.video_info = self.youtube.videos().list(id=self.video_id, part='snippet,statistics').execute()
+        self.title = self.video_info["items"][0]["snippet"]["title"]
+        self.url = f"https://www.youtube.com/{self.video_id}"
+        self.view_count = self.video_info["items"][0]["statistics"]["viewCount"]
+        self.like_count = self.video_info["items"][0]["statistics"]["likeCount"]
+
+    def __str__(self):
+        return self.title
+
+class PLVideo(Video):
+    """Класс для видео в плейлисте"""
+
+    def __init__(self, video_id: str, playlist_id: str) -> None:
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
